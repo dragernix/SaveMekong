7cbdde79ed595f3e846fecceb958ac1bdd454cea
jasmineseah-17 <seah.yongyee.jasmine@dhs.sg>
2018-03-25 12:59:38 +0800

    Add files via upload

---
+++ unesco_hackathon/index.php
@@ -0,0 +1,166 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Save Mekong</title>
+<!--
+Short HTML Template
+http://www.templatemo.com/tm-502-short
+-->
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/animate.min.css">
+        <link rel="stylesheet" href="css/easy-responsive-tabs.min.css">
+        <link rel="stylesheet" href="css/tabs.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+    <!-- Laoding page  -->
+    <div class="preloader">
+      <div class="spinner">
+        <div class="dot1"></div>
+        <div class="dot2"></div>
+      </div>
+    </div>
+    <!-- End Laoding page  -->
+
+    <!-- Begin HorizontalTab style 6 -->
+    <section class="section-full image-bg">
+
+      <div class="container">
+
+        <div class="row">
+
+          <div class="col-md-12">
+
+            <a href="index.php"><div class="responsive-logo hidden-lg hidden-md hidden-sm"><img src="img/logo.png"></div></a>
+            
+            <!-- Begin .HorizontalTab -->
+            <div class="VerticalTab VerticalTab_hash_scroll VerticalTab_6 tabs_ver_6">
+            
+              <ul class="resp-tabs-list hor_1">
+                <a href="index.php"><div class="logo"><img src="img/logo.png"></div></a>
+                <li class="tabs-1" data-tab-name="intro"><span class="tabs-text">Intro to Mekong</span></li>
+                <li class="tabs-2" data-tab-name="about"><span class="tabs-text">About Us</span></li>
+                <li class="tabs-3" data-tab-name="information"><span class="tabs-text">Inforgraphics</span></li>
+              </ul>
+              
+              <div class="resp-tabs-container hor_1 tabs_scroll">
+                
+                <div class="fc-tab-1">
+                
+                  <div class="home-container">
+                    <div class="row">
+                      <div class="col-md-6">
+                        <div class="left-content">
+                          <div class="left-line"></div>  
+                          <h2>Sustainable Mekong </h2>
+                          <p>The Mekong is a trans-boundary river in Southeast Asia</p>
+                          <div class="primary-button">
+                            <a href="https://en.wikipedia.org/wiki/Mekong">Discover More</a>
+                          </div>
+                          
+                        </div>
+                      </div>
+                      <div class="col-md-6">
+                        <div class="right-content">
+                          <img src="img/home-mekong.jpg" width="370" height="390">
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  
+                </div>
+                
+                <div class="fc-tab-2" >
+                
+                  <div class="about-container">
+                    <div class="row">
+                      <div class="col-md-12">
+                        <h2>More info about us</h2>
+                        <div class="under-line"></div>
+                        <p>We are a group of students working to create a sustainable plan for Mekong</p>
+                        <img src="img/about-image.jpg">
+                      </div>
+                    </div>
+                  </div>
+                  
+                </div>
+                
+                <div class="fc-tab-3">
+
+                  <div class="about-container lowerbottom">
+                    <div>
+                    <div class='tableauPlaceholder' id='viz1521953181793' style='position: relative'><noscript><a href='#'><img alt='Dashboard 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Me&#47;MekongCountriesCarbonEmissions&#47;Dashboard1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='MekongCountriesCarbonEmissions&#47;Dashboard1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Me&#47;MekongCountriesCarbonEmissions&#47;Dashboard1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1521953181793');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='1000px';vizElement.style.height='827px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
+                      <div class='left-content'>
+                      <div class = 'primary-button'>
+                        <a href = 'suggestions.php'>Learn more about what you can do to help Mekong here</a>
+                      </div>
+                      </div>
+                    </div>
+                    
+                  </div>
+                  
+                </div>
+                
+                
+                
+              </div>
+            </div>
+            <!-- End .HorizontalTab -->
+          
+          </div>
+          
+        </div>
+        
+      </div>
+
+    </section>
+    <!-- End HorizontalTab style 6 -->
+    
+
+      <footer>
+        <p>Copyright &copy; 2018 Save Mekong 
+                                
+        | Design: <a href="http://www.templatemo.com" target="_parent"><em>template mo</em></a></p>
+      </footer>
+    </div> <!-- /container -->        
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+        <script src="js/jquery.nicescroll.min.js"></script>
+        <script src="js/easyResponsiveTabs.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    
+
+    </body>
+</html>
---
+++ unesco_hackathon/suggestions.php
@@ -0,0 +1,214 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Save Mekong</title>
+<!--
+Short HTML Template
+http://www.templatemo.com/tm-502-short
+-->
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/animate.min.css">
+        <link rel="stylesheet" href="css/easy-responsive-tabs.min.css">
+        <link rel="stylesheet" href="css/tabs.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+        
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+    <!-- Laoding page  -->
+    <div class="preloader">
+      <div class="spinner">
+        <div class="dot1"></div>
+        <div class="dot2"></div>
+      </div>
+    </div>
+    <!-- End Laoding page  -->
+
+    <!-- Begin HorizontalTab style 6 -->
+    <section class="section-full image-bg">
+
+      <div class="container">
+
+        <div class="row">
+
+          <div class="col-md-12">
+
+            <a href="index.php"><div class="responsive-logo hidden-lg hidden-md hidden-sm"><img src="img/logo.png"></div></a>
+            
+            <!-- Begin .HorizontalTab -->
+            <div class="VerticalTab VerticalTab_hash_scroll VerticalTab_6 tabs_ver_6">
+            
+              <ul class="resp-tabs-list hor_1">
+                <a href="index.php"><div class="logo"><img src="img/logo.png"></div></a>
+                <li class="tabs-1" data-tab-name="suggestion"><span class="tabs-text">Suggestion</span></li>
+                <li class="tabs-2" data-tab-name="addsuggestions"><span class="tabs-text">Add suggestions</span></li>
+                <li class="tabs-3" data-tab-name="emailrelevant"><span class="tabs-text">Email relevant authorities</span></li>
+              </ul>
+              
+              <div class="resp-tabs-container hor_1 tabs_scroll">
+                
+                <div class="fc-tab-1">
+                
+                  <div class="home-container">
+                    <div class="row">
+                      <div class="col-md-6">
+                        <div class="left-content">
+                          <div class="left-line"></div>  
+                          <h2>List of Suggestions </h2><p><ul>
+                          <?php
+                            require_once "SuggestionDao.php";
+                            $dao = new SuggestionDAO();
+                            if(count($dao->getSuggestions()) >0){
+                                echo "<ul>";
+                                foreach ($dao->getSuggestions() as $item){
+                                    echo "<li>";
+                                    echo $item;
+                                    echo "</li>";
+                                }
+                                echo "</ul>";
+                            }
+                            else{
+                                echo "There are no suggestions yet :(";
+                            }
+                        ?>
+                        </ul>
+                        </p>
+                        </div>
+                      </div>
+                      <div class="col-md-6">
+                        <div class="right-content">
+                          <div class='tableauPlaceholder' id='viz1521952292316' style='position: relative'><noscript><a href='#'><img alt='Sheet 4 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Me&#47;MekongCountriesCarbonEmissions&#47;Sheet4&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='MekongCountriesCarbonEmissions&#47;Sheet4' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Me&#47;MekongCountriesCarbonEmissions&#47;Sheet4&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1521952292316');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
+                      
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  
+                </div>
+                
+                <div class="fc-tab-2" >
+                
+                  <div class="about-container lowerbottom">
+                    <div class="row">
+                      <div class="col-md-12">
+                        <h2>Add suggestions</h2>
+                        <div class="under-line"></div>
+                        <form action = 'processSuggestion.php' method = 'GET'> 
+                            Suggestion: <input type = text name='suggest'/>
+                            <input type = submit value ='Submit'/>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                  
+                </div>
+                
+                <div class="fc-tab-3" >
+                
+                <div class="home-container lowerbottom">
+                    <div class="row">
+                      <div class="col-md-6">
+                        <div class="left-content">
+                          <div class="left-line"></div>  
+                          <h2>List of Suggestions </h2><p><ul>
+                          <?php
+                            require_once "SuggestionDao.php";
+                            $dao = new SuggestionDAO();
+                            echo "<form method = 'GET' action = 'processEmails.php'>";
+                            if(count($dao->getSuggestions()) >0){
+                                echo "<table>";
+                                foreach ($dao->getSuggestions() as $item){
+                                    echo "<tr><td>";
+                                    echo $item;
+                                    echo "</td><td>";
+                                    echo "<input type = 'checkbox' name='toEmail[]' value ='$item'/";
+                                    echo "</td></tr>";
+                                }
+                                echo "</table>";
+                            }
+                            else{
+                                echo "There are no suggestions yet :(";
+                            }
+                        ?>
+                        </ul>
+                        </p>
+                        </div>
+                      </div>
+                      <div class="col-md-6">
+                        <div class="left-content">
+                            <div class="left-line"></div>  
+                          <h2>List of Relevant Authorities </h2><p><ul>
+                              <table><tr><td>Relevant authority 1</td><td><input type = 'checkbox' name='authority[]' value = 'chuamingyu@yahoo.com.sg'></td></tr>
+                                <tr><td>Relevant authority 2</td><td><input type='checkbox' name='authority[]' value = 'lim.jinyang@dhs.sg'></td></tr>
+                            </table>
+                        </div>
+                      </div>
+                    </div>
+                    <input type = 'submit' value = 'Email selected items' class = 'test'/>
+                  </div>
+                  
+                  
+                  
+                </div>
+                
+                
+              </div>
+            </div>
+            <!-- End .HorizontalTab -->
+          
+          </div>
+          
+        </div>
+        
+      </div>
+
+    </section>
+    <!-- End HorizontalTab style 6 -->
+    
+
+      <footer>
+        <p>Copyright &copy; 2018 Save Mekong 
+                                
+        | Design: <a href="http://www.templatemo.com" target="_parent"><em>template mo</em></a></p>
+      </footer>
+    </div> <!-- /container -->        
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+        <script src="js/jquery.nicescroll.min.js"></script>
+        <script src="js/easyResponsiveTabs.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    
+
+    </body>
+</html>
